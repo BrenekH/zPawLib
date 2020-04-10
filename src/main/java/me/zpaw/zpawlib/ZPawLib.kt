4f5bb32f60cf865e9bf67a2a8141272bdb1b3b7e
Brenek Harrison <brenekharrison@gmail.com>
2020-04-09 20:17:52 -0600

    .coloredName

--- src/main/java/me/zpaw/zpawlib/ZPawLib.kt
+++ src/main/java/me/zpaw/zpawlib/ZPawLib.kt
@@ -3,6 +3,7 @@ package me.zpaw.zpawlib
 import me.zpaw.zpawlib.data.ConfigManager
 import me.zpaw.zpawlib.dependencies.Metrics
 import me.zpaw.zpawlib.eventlisteners.PlayerJoinEventListener
+import org.bukkit.ChatColor
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.*
 
@@ -19,8 +20,8 @@ class ZPawLib: JavaPlugin() {
         playerCache.saveConfig()
     }
 
-    fun getPlayer(name: String): ZPlayer? {
+    fun getPlayer(name: String, color: ChatColor): ZPlayer? {
         val playerUUID: UUID = UUID.fromString((playerCache.config.get("players.$name.uuid", null) ?: return null) as String?)
-        return ZPlayer(playerUUID)
+        return ZPlayer(playerUUID, name, color)
     }
 }
--- src/main/java/me/zpaw/zpawlib/ZPlayer.kt
+++ src/main/java/me/zpaw/zpawlib/ZPlayer.kt
@@ -1,13 +1,16 @@
 package me.zpaw.zpawlib
 
 import org.bukkit.Bukkit
+import org.bukkit.ChatColor
 import org.bukkit.OfflinePlayer
 import org.bukkit.entity.Player
 import java.util.UUID
 
-class ZPlayer(uuid: UUID) {
+class ZPlayer(uuid: UUID, name: String, nameColor: ChatColor) {
     val player: OfflinePlayer = Bukkit.getOfflinePlayer(uuid)
     var onlinePlayer: Player? = null
+
+    val coloredName: String = "$nameColor$name${ChatColor.RESET}"
 
     init {
         if (Bukkit.getPlayer(uuid) != null)
